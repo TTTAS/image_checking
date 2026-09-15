6854ac77c6c63d2c02cc4537577709689419ef6d
TTTAS <122945956+TTTAS@users.noreply.github.com>
2026-09-15 13:28:10 +0800

    Show the same wallpaper actions for videos

--- flutter_src/lib/src/viewer.dart
+++ flutter_src/lib/src/viewer.dart
@@ -175,16 +175,19 @@ class _ViewerPageState extends State<ViewerPage> {
           overflow: TextOverflow.ellipsis,
         ),
         actions: [
-          if (_current.type != AssetType.video)
-            PopupMenuButton<String>(
+          PopupMenuButton<String>(
               icon: const Icon(Icons.wallpaper),
               tooltip: '桌布',
               onSelected: (v) {
                 switch (v) {
                   case 'single':
-                    Navigator.of(context).push(MaterialPageRoute<void>(
-                      builder: (_) => WallpaperCropPage(asset: _current),
-                    ));
+                    if (_current.type == AssetType.video) {
+                      _addToPlaylist([WallpaperTarget.home]);
+                    } else {
+                      Navigator.of(context).push(MaterialPageRoute<void>(
+                        builder: (_) => WallpaperCropPage(asset: _current),
+                      ));
+                    }
                     break;
                   case 'home':
                     _addToPlaylist([WallpaperTarget.home]);
